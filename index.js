e851f2d1627dc16916a101c8a218a3b36d9e6d45
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-12 22:59:41 +0700

    bot-test

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
 
-app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.json())
 
-app.use(express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 8000))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.post('/webhook', (req, res) => {
+  var text = req.body.events[0].message.text
+  var sender = req.body.events[0].source.userId
+  var replyToken = req.body.events[0].replyToken
+  console.log(text, sender, replyToken)
+  console.log(typeof sender, typeof text)
+  // console.log(req.body.events[0])
+  if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
+    sendText(sender, text)
+  }
+  res.sendStatus(200)
+})
 
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
+function sendText (sender, text) {
+  let data = {
+    to: sender,
+    messages: [
+      {
+        type: 'text',
+        text: 'สวัสดีค่ะ เราเป็นผู้ช่วยปรึกษาด้านความรัก สำหรับหมามิ้น 💞'
+      }
+    ]
+  }
+  request({
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer {'+ YV4YHUNnJhTURSd9BzLGokn7ALa8+pKl/KooSoAEW7CL4yNF9TwjC3Jw5TuivsoQ3VwhB87kTwCamwcHFHj0Qv6XGMZKbJYXziekYqmHFnBj9AvZpxya3rRNupun8JIFv5EzUZUPlfZcywrvH9jhgQdB04t89/1O/w1cDnyilFU= +'}'
+    },
+    url: 'https://api.line.me/v2/bot/message/push',
+    method: 'POST',
+    body: data,
+    json: true
+  }, function (err, res, body) {
+    if (err) console.log('error')
+    if (res) console.log('success')
+    if (body) console.log(body)
+  })
+}
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-//(╯°□°)╯︵ ʞooqǝɔɐɟ
-//(し ゜-゜)し facebook
+app.listen(app.get('port'), function () {
+  console.log('run at port', app.get('port'))
+})
