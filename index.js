8f5e7fb69f04bdec0caae01dbd44ab2b1e332c6b
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-13 15:18:35 +0700

    8-commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var request = require('request');
 var app = express();
 
 app.use(bodyParser.json());
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 4000));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
