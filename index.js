a02f46e95cc33e6bb1b827a87a652572efd1cb03
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-13 15:20:10 +0700

    9-commit

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ app.post('/webhook', (req, res) => {
   var text = req.body.events[0].message.text
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
+  console.log(text, sender, replyToken);
+  console.log(typeof sender, typeof text);
   // console.log(req.body.events[0])
   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
+    sendText(sender, text);
   }
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 function sendText (sender, text) {
   let data = {
@@ -51,10 +51,10 @@ function sendText (sender, text) {
     body: data,
     json: true
   }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
+    if (err) console.log('error');
+    if (res) console.log('success');
+    if (body) console.log(body);
+  });
 }
 
 
