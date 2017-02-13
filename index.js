aae9731102c222a6f91b4390c2b0afe9f84b26e4
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-13 14:44:17 +0700

    two-commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express()
 
 app.use(bodyParser.json())
 
-app.set('port', (process.env.PORT || 8000))
+app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
