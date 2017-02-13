55c84e20f199ec8caf8b99ff949d236b6fac5774
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-13 15:13:09 +0700

    5-commit

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var bodyParser = require('body-parser');
 var request = require('request');
 var app = express();
 
+app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
