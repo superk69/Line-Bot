7f3f5d55d6a74c9956ff08c3b45bdf8c994fa845
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-13 14:55:09 +0700

    two-commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 5000))
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.set('port', (process.env.PORT || 5000));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello Index.js</h1>');
@@ -17,14 +17,14 @@ app.post('/webhook', (req, res) => {
   var text = req.body.events[0].message.text
   var sender = req.body.events[0].source.userId
   var replyToken = req.body.events[0].replyToken
-  console.log(text, sender, replyToken)
-  console.log(typeof sender, typeof text)
+  console.log(text, sender, replyToken);
+  console.log(typeof sender, typeof text);
   // console.log(req.body.events[0])
   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
+    sendText(sender, text);
   }
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 function sendText (sender, text) {
   let data = {
@@ -46,12 +46,12 @@ function sendText (sender, text) {
     body: data,
     json: true
   }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
+    if (err) console.log('error');
+    if (res) console.log('success');
+    if (body) console.log(body);
+  });
 }
 
 app.listen(app.get('port'), function () {
-  console.log('run at port', app.get('port'))
-})
+  console.log('run at port', app.get('port'));
+});
