eec88d3903df5f4b85d503d14b71afa2159c653f
Yonahha <kritsana.ch.57@ubu.ac.th>
2017-02-14 12:16:38 +0700

    test-weather-js-ๅ

--- index.js
+++ index.js
@@ -61,7 +61,11 @@ function sendText (sender, text) {
   });
 }
 
+weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
+  if(err) console.log(err);
 
+  console.log(JSON.stringify(result, null, 2));
+});
 
 
 app.listen(app.get('port'), function() {
